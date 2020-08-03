6053a17fa1a20f1462ad40ad839fa0f382e219a3
“Megan <meganschertz@gmail.com>
2020-08-03 14:54:36 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,38 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    page = index.css("div.student-card")
+    page.collect do |student|
+      {:name => student.css(".student-name").text ,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attr('href').value
+      }
+    end
   end
+
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    page = Nokogiri::HTML(open(profile_url))
+    student_profile = {}
 
+    container = page.css("div.main-wrapper.profile .social-icon-container a")
+    container.each do |element| 
+      if element.attr('href').include?("twitter")
+        student_profile[:twitter] = element.attr('href')
+      elsif element.attr('href').include?("linkedin")
+        student_profile[:linkedin] = element.attr('href')
+      elsif element.attr('href').include?("github")
+        student_profile[:github] = element.attr('href')
+      elsif element.attr('href').end_with?("com/")
+        student_profile[:blog] = element.attr('href')
+      end
+    end
+    student_profile[:profile_quote] = page.css(".vitals-container .vitals-text-container .profile-quote").text
+    student_profile[:bio] = page.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+
+    student_profile
+  end
 end
 
